52eea75d65d29305aa0ea22e0ce7551d99614561
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2021-10-18 12:26:05 +0200

    complete working JWT

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
-const secret = process.env.JWT_SECRET;
+const port = process.env.PORT || 3000;
+const secret = /*Math.random() + */ process.env.JWT_SECRET;
 
 app.get("/token", (req, res) => {
-    const payload = { userId: 42 };
-    const token = jwt.sign(payload, secret);
+    const payload = { userId: 42, username: "Veera Cat", admin: "true" };
+    const options = { expiresIn: "1h" };
+
+    const token = jwt.sign(payload, secret, options);
     res.send(token);
 });
 
@@ -21,21 +24,16 @@ const checkToken = (req, res, next) => {
     if (!tokenToCheck) res.status(401);
 
     jwt.verify(tokenToCheck, secret, (err, payload) => {
-        console.log(err, payload);
-
-        if (err) {
-            return res.status(400).send(err.message);
-        }
+        if (err) res.status(400).send(err.message);
+        req.profile = payload;
         next();
     });
 };
 
 app.get("/secure", checkToken, (req, res) => {
-    // check token and return something
-    res.send("Access granted!!");
+    res.send(`Access to ${req.profile.username} granted.`);
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
 });
