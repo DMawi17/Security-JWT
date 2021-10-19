298dc296382433ddc403e4cc735b8d2fd514d00f
Dagmawi A <75326045+DMawi17@users.noreply.github.com>
2021-10-19 17:30:03 +0200

    Remove mongoUri

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ const config = {
     port: process.env.PORT || 3000,
     secret: process.env.JWT_SECRET,
     mongoUri:
-        "mongodb://classTester:1to5@localhost:27017/?authSource=test&readPreference=primary&ssl=false",
+        
 };
 
 export default config;
